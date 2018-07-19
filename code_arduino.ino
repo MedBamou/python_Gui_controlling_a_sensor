2532965d72718d3739d917cf690604095cbb44f6
MedBamou <41172808+MedBamou@users.noreply.github.com>
2018-07-19 20:32:44 +0100

    Add files via upload

---
+++ code_arduino.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+ 
+Servo myservo;  
+ 
+int pos = 90;    
+ 
+void setup() {
+        Serial.begin(9600); 
+        myservo.attach(9); 
+}
+ 
+void loop() {
+ 
+        while (Serial.available() > 0) {
+                int c = Serial.read();
+                delay(2); 
+                
+                Serial.print("C is ");
+                Serial.println(c, DEC);
+                myservo.write(c);
+        }
+}
